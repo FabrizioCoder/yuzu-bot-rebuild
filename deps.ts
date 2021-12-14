b9615d3b2eead86f873fb4e922916df6f0152cab
Le-Val <le-val1@protonmail.ch>
2021-12-14 15:33:55 -0500

    working on context menus

--- deps.ts
+++ deps.ts
@@ -2,15 +2,15 @@ export * from "https://deno.land/x/discordeno@13.0.0-rc15/mod.ts";
 
 // NOTE: import only the 'enableCachePlugin' function do not import anything else
 export {
-  enableCachePlugin
+  enableCachePlugin,
 } from "https://deno.land/x/discordeno_cache_plugin@0.0.18/mod.ts";
 
 export type {
-  BotWithCache
+  BotWithCache,
 } from "https://deno.land/x/discordeno_cache_plugin@0.0.18/mod.ts";
 
 // NOTE: import this module as a standalone piece do not import the mod.ts
 export {
+  botHasGuildPermissions,
   hasGuildPermissions,
-  botHasGuildPermissions
 } from "https://deno.land/x/discordeno_permissions_plugin@0.0.11/src/permissions.ts";
