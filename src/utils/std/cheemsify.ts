53e9853950152cd722b3a0d9bbdfa840aab3cc40
Le-Val <le-val1@protonmail.ch>
2021-12-14 21:41:05 -0500

    small fixes

--- src/utils/std/cheemsify.ts
+++ src/utils/std/cheemsify.ts
@@ -3,14 +3,18 @@
  * Ems puram y sin simdem emffemcts
  */
 export function cheemsify(text: string) {
+  // reemplamzam por eml nuemvom temxtom momdimfimcamdo
   return text.replace(/[aáeéiíoóuú]+[^aáeéiíoóuúmnñry]/gi, (value) => {
+    // premfimx simgnimfcam eml string y pomstfimx eml úmltimom caramcter
     const prefix = value.slice(0, value.length - 1);
     const postfix = value[value.length - 1];
 
+    // infimx ems lom quem vam enmemdiom (om seam lam m)
     const infix = (
       prefix.toUpperCase() === prefix ? "M" : "m"
     );
 
+    // finamlmentem vomlvemoms am unir loms strings
     return prefix + infix + postfix;
   });
 }
