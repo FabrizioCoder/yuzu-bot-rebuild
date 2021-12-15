d227eb00922ee7faef00809f67ab9645641c13ad
Le-Val <le-val1@protonmail.ch>
2021-12-15 10:46:10 -0500

    fix

--- src/utils/constants/option.ts
+++ src/utils/constants/option.ts
@@ -4,7 +4,7 @@ export const Options = {
   GUILD_ID: 891367004903182336n,
   OWNER_ID: 774292293020155906n,
   // application id
-  ID: 904922230285291531n, // main bot
+  ID: 893319907981283340n, // main bot
   // ETC
   PREFIX: "!",
   TOKEN: "........",
