78785c494b42f9c1cc9e7386b4f404c2a522a6db
Le-Val <le-val1@protonmail.ch>
2021-12-17 12:44:35 -0500

    .

--- src/tasks/uptime.ts
+++ src/tasks/uptime.ts
@@ -8,9 +8,9 @@ export default <Task> {
   async execute(bot, _payload, uptime) {
     const uptimeMessage = `
     El bot ha estado encendido desde:
-    <t:${Math.floor(uptime)}:R>
-    <t:${Math.floor(uptime)}:F>
-    <t:${Math.floor(uptime)}:d>
+    <t:${Math.floor(uptime / 1000)}:R>
+    <t:${Math.floor(uptime / 1000)}:F>
+    <t:${Math.floor(uptime / 1000)}:d>
     `;
 
     const channelId = Options.CHANNEL_ID;
