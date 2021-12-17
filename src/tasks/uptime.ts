40aa3abba3e170ad2b26baccedde99b2cb5ea19b
Le-Val <le-val1@protonmail.ch>
2021-12-17 14:20:50 -0500

    fix: tralling spaces

--- src/tasks/uptime.ts
+++ src/tasks/uptime.ts
@@ -2,15 +2,14 @@ import type { Task } from "../types/task.ts";
 import { Milliseconds, Options } from "../utils/mod.ts";
 import { sendMessage } from "../../deps.ts";
 
-export default <Task> {
+export default <Task>{
   name: "uptime",
   interval: Milliseconds.HOUR,
   async execute(bot, _payload, uptime) {
-    const uptimeMessage = `
-      El bot ha estado encendido desde:
-      -> <t:${Math.floor(uptime / 1000)}:R>
-      -> <t:${Math.floor(uptime / 1000)}:F>
-    `.trim();
+    const uptimeMessage =
+      "El bot ha estado encendido desde:\n" +
+      `-> <t:${Math.floor(uptime / 1000)}:R>\n` +
+      `-> <t:${Math.floor(uptime / 1000)}:F>`;
 
     // log
     await sendMessage(bot, Options.CHANNEL_ID, uptimeMessage);
