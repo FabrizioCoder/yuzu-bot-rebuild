0329397ac60d128f67d7af2e5087c72aabadba78
Le-Val <le-val1@protonmail.ch>
2022-01-04 14:42:48 -0500

    working on v2

--- src/oasis/mod.ts
+++ src/oasis/mod.ts
@@ -3,7 +3,7 @@ export * from "./types/command.ts";
 
 // helpers
 export * from "./decorators/command.ts";
-export * from "./decorators/slash_command_permissions.ts";
+export * from "./handler.ts";
 export * from "./decorators/before.ts";
 export * from "./decorators/stop.ts";
 export * from "./decorators/overwrite.ts";
