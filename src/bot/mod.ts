2c63df32b4c628829ef585deb787f782a0bcca6c
Le-Val <le-val1@protonmail.ch>
2022-01-11 18:20:11 -0500

    the bot relies on permission plugin!

--- src/bot/mod.ts
+++ src/bot/mod.ts
@@ -2,6 +2,7 @@ import { Configuration, loadDynamicCommands } from "utils";
 import { cache, loadFilesFromBot } from "oasis";
 import { createBot, startBot } from "discordeno";
 import { enableCachePlugin } from "cache_plugin";
+import { enablePermissionsPlugin } from "permissions_plugin";
 import { startDatabase } from "../database/db.ts";
 
 import "dotenv/load";
@@ -17,16 +18,18 @@ loadDynamicCommands();
 
 // start the bot
 await startBot(
-  enableCachePlugin(
-    createBot({
-      botId: Deno.args[0] ? BigInt(Deno.args[0]) : Configuration.ID,
-      intents: ["Guilds", "GuildMessages", "GuildMessageReactions", "GuildEmojis", "DirectMessages"],
-      events: Object.fromEntries(
-        // transforms a Map<string, T> into a Record<string, T["execute"]>
-        Array.from(cache.events.entries(), ([name, event]) => [name, event.execute])
-      ),
-      token: Deno.env.get("TOKEN") ?? Configuration.TOKEN,
-    })
+  enablePermissionsPlugin(
+    enableCachePlugin(
+      createBot({
+        botId: Deno.args[0] ? BigInt(Deno.args[0]) : Configuration.ID,
+        intents: ["Guilds", "GuildMessages", "GuildMessageReactions", "GuildEmojis", "DirectMessages"],
+        events: Object.fromEntries(
+          // transforms a Map<string, T> into a Record<string, T["execute"]>
+          Array.from(cache.events.entries(), ([name, event]) => [name, event.execute])
+        ),
+        token: Deno.env.get("TOKEN") ?? Configuration.TOKEN,
+      })
+    )
   )
 );
 
