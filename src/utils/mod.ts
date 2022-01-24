a45f8af907993c76f5e506c51d67dd39c580a71d
Le-Val <le-val1@protonmail.ch>
2022-01-24 15:48:57 -0500

    :

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -3,7 +3,6 @@ export * from "./constants.ts";
 
 // utility
 export * from "./std/range.ts";
-export * from "./std/snowflake.ts";
 export * from "./std/cheemsify.ts";
 export * from "./std/logger.ts";
 export * from "./algorithms/levenshtein_distance.ts";
