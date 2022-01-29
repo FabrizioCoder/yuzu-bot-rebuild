a0935e18f37d8086d3694fc9c5634e7f7b2b916c
Yuzu <yuzuru@programmer.net>
2022-01-28 19:20:19 -0500

    pog

--- src/bot/commands/basic/search.ts
+++ src/bot/commands/basic/search.ts
@@ -2,6 +2,8 @@ import { createCommand, MessageApplicationCommandBuilder } from "oasis";
 import { Category } from "utils";
 import { SafetyLevels, search } from "images";
 
+const disabled = true as boolean;
+
 createCommand({
   meta: {
     descr: "commands:search:DESCRIPTION",
@@ -10,6 +12,10 @@ createCommand({
   category: Category.Util,
   translated: true,
   async execute({ interaction }) {
+    if (disabled) {
+      return "The owner has restricted this command to a whitelist! feel free to ask for access on the support server";
+    }
+
     const message = interaction.data?.resolved?.messages?.first();
 
     if (!message) {
